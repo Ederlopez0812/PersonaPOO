04d52e96b14e8db9002a50d03bed0c981561555d
Eder Lopez <ederlopez8820@gmail.com>
2024-04-29 19:31:56 -0600

    modificadores

--- Persona.java
+++ Persona.java
@@ -1,9 +1,53 @@
 public class Persona {
 
-    String nombre;
-    String apellido;
-    int edad;
+    private String nombre;
+    private String apellido;
+    private int edad;
     double altura;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if (edad<0) {
+            System.out.println(" edad es invilida");
+            
+        }else{
+            this.edad = edad;
+        }
+        
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        if (altura<0) {
+            System.out.println("altura invalida");
+            
+        } else{
+            this.altura = altura;
+        }
+        
+    }
 
     public void mostrarNombre() {
         System.out.println(" mi nombre es: " + this.nombre);
@@ -17,6 +61,14 @@ public class Persona {
 
     public void mostrarAltura(){
         System.out.println("mi altura es: " +this.altura);
+      
     }
+
+    @Override
+    public String toString() {
+        return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", altura=" + altura + "]";
+        
+    }
+
 
 }
